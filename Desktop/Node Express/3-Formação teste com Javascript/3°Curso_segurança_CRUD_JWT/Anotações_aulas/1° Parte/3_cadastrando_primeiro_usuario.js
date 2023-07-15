6bb284bc3642cd1ef4d7c9c189acf6c56bd4a274
gabriel-henrique-oliveira-amaral-MB <gabrielmanoelbatista@gmail.com>
2023-07-15 12:50:03 -0300

    1 parte criando rota de login

--- Desktop/Node Express/3-Formação teste com Javascript/3°Curso_segurança_CRUD_JWT/Anotações_aulas/1° Parte/3_cadastrando_primeiro_usuario.js
+++ Desktop/Node Express/3-Formação teste com Javascript/3°Curso_segurança_CRUD_JWT/Anotações_aulas/1° Parte/3_cadastrando_primeiro_usuario.js
@@ -6,3 +6,6 @@
 // Necessário um cuidado muito milindroso ao mecher com requisições no sequelize, conferir as tabelas, pois qualquer erro pode ser fatal para aplicação
 
 // Vou usar branch para melhorar o entendimento da aplicação mas voltarei uma pasta para criá-la
+// Dentro de auth podemos criar nossas rotas de seguranças
+// Vamos também em index importar nossas rotas de login 
+// Precisamos também criar um novo arquivo controller
---
+++ Desktop/Node Express/3-Formação teste com Javascript/3°Curso_segurança_CRUD_JWT/Anotações_aulas/2° Parte/1_criando_rota_login.js
@@ -0,0 +1,3 @@
+// Já temos todas nossas funções criadas, precisamos de um módulo de segurança a mais que é login, pois todas as rotas estão publicas
+// Para isso dentro de routes vamos criar um arquivo authRoutes.js
+// Preciamos também criar nossa service
